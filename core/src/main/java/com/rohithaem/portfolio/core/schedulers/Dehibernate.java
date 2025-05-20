d51d9ff1aa330aeec23ebbb6863de4670a4bc34d
RohithV16 <Rohithv16@gmail.com>
2025-05-20 19:47:43 +0530

    Dehibernate class

---
+++ core/src/main/java/com/rohithaem/portfolio/core/schedulers/Dehibernate.java
@@ -0,0 +1,58 @@
+package com.rohithaem.portfolio.core.schedulers;
+
+import com.rohithaem.portfolio.core.commons.ResourceResolverService;
+
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.discovery.TopologyEvent;
+import org.apache.sling.discovery.TopologyEventListener;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(
+        service = Runnable.class,
+        immediate = true, // Load immediately
+        property = {
+                "scheduler.expression=*/10 * * * * ?",
+                "scheduler.concurrent=false",
+                "scheduler.runOn=LEADER"
+        }
+)
+public class Dehibernate implements Runnable, TopologyEventListener {
+    private final Logger log = LoggerFactory.getLogger(Dehibernate.class);
+
+    @Reference
+    private ResourceResolverService resourceResolverService;
+    private boolean isLeader = false;
+
+    @Override
+    public void run() {
+        // Scheduled services that do not have to be cluster aware do not need
+        // to implement this check OR extend TopologyEventListener
+        if (!isLeader) {
+            return;
+        }
+
+        // Scheduled service logic, only run on the Master
+        ResourceResolver resolver = null;
+        try {
+            resolver = resourceResolverService.getResourceResolver();
+
+            log.error("It is running on author {}", resolver.isLive());
+
+        } finally {
+            if (resolver != null) {
+                resolver.close();
+            }
+        }
+    }
+
+    @Override
+    public void handleTopologyEvent(final TopologyEvent event) {
+        if (event.getType() == TopologyEvent.Type.TOPOLOGY_CHANGED
+                || event.getType() == TopologyEvent.Type.TOPOLOGY_INIT) {
+            this.isLeader = event.getNewView().getLocalInstance().isLeader();
+        }
+    }
+}
